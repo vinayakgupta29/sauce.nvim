49231c19c19e241f9f636cf7c0306ad6b90261b1
Vinayak <vinayakg236@gmail.com>
2026-07-30 21:19:43 +0530

    updated the init again with the new API

--- lua/sauce/init.lua
+++ lua/sauce/init.lua
@@ -1,23 +1,49 @@
 local M = {}
 
+local function parser_extension()
+  if vim.uv.os_uname().sysname == "Windows_NT" then
+    return "dll"
+  elseif vim.uv.os_uname().sysname == "Darwin" then
+    return "dylib"
+  else
+    return "so"
+  end
+end
+
+local function ensure_parser_installed()
+  local plugin = debug.getinfo(1, "S").source:sub(2)
+  plugin = vim.fs.dirname(vim.fs.dirname(plugin))
+
+  local ext = parser_extension()
+  local parser = plugin .. "/parser/saucelang." .. ext
+
+  if vim.uv.fs_stat(parser) then
+    return true
+  end
+
+  vim.notify("Sauce.nvim: building Tree-sitter parser...", vim.log.levels.INFO)
+
+  local result = vim.system(
+    { "make", "nvim" },
+    { cwd = plugin }
+  ):wait()
+
+  if result.code ~= 0 then
+    vim.notify(result.stderr, vim.log.levels.ERROR)
+    return false
+  end
+
+  return vim.uv.fs_stat(parser) ~= nil
+end
+
 function M.setup()
-  -- register parser
-  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-  parser_config.saucelang = {
-    install_info = {
-      url = vim.fn.stdpath("data") .. "/lazy/sauce.nvim",
-      files = { "src/parser.c" },
-    },
-    filetype = "saucelang",
-  }
-
-  -- filetype detection
   vim.filetype.add({
     filename = {
       ["sauce.txt"] = "saucelang",
     },
   })
+
+  ensure_parser_installed()
 end
 
 return M
